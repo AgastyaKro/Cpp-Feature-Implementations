a82ab90ea5ae27ad6d942488e04de850ab09e997
Agastya Krothapalli <agastyakro06@gmail.com>
2026-03-23 17:02:09 -0400

    Finixed Futex

--- FutexMutex.cpp
+++ FutexMutex.cpp
@@ -1,6 +1,9 @@
 #include <atomic>
 #include <sys/syscall.h>
 #include <linux/futex.h>
+#include <unistd.h>
+#include <cerrno>
+
 
 
 
@@ -9,7 +12,26 @@ class FutexMutex {
 public:
     FutexMutex() : state_(0) {}
 
+    void lock(){
+        int expected = 0;
+        // fast path
+        if (state_.compare_exchange_strong(expected, 1, std::memory_order_acquire, std::memory_order_relaxed)){
+            return;
+        }
+        // slow path
+        for (;;){
+            if (state_.exchange(2, std::memory_order_acquire) == 0){
+                return;
+            }
+            futex_wait(&state_, 2);
+        }
+    }
 
+    void unlock(){
+        if (state.exchange(0, std::memory_order_release) == 2){
+            futex_wake(&state_, 1);
+        }
+    }
 
 
 private:
